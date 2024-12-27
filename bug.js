bc67ca291fef76e00e84ce5d58eeb51cad4cecc1
BugHunter-X <askzenai@gmail.com>
2024-12-27 14:52:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+//In your main app.js file
+import 'tailwindcss/tailwind.css';
+
+//your app jsx code
+```
+
+```javascript
+//In your component's jsx file
+<div className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    <p>Hover over me!</p>
+</div>
+```
